650fa5346a00f608fb4c0b745c40d4d5b8221f0c
mahmoudhindam9-stack <mahmoudhindam9@gmail.com>
2026-09-17 09:00:14 +0300

    chore(release): bump version to 1.3.1 for updater fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
     applicationId = "com.example.mediaaistudio"
     minSdk = 24
     targetSdk = 36
-    versionCode = 4
-    versionName = "1.3.0"
+    versionCode = 5
+    versionName = "1.3.1"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
